1466574628e0cf4ac8dbe30984e7a2ceef6b8238
Joachim Kasper <kasper@iMac5K.local>
2022-12-06 21:31:34 +0100

    Copy non git

---
+++ _includes/lnbits-balance.php
@@ -0,0 +1,22 @@
+<?php
+
+$url = "https://lnbits.satoshibox.io/api/v1/wallet";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "X-Api-Key: ffa80c8a67504680bbea48765dd19410",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
+print = "hello";
+
+?>
