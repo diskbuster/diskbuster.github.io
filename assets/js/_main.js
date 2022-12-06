757a42c4c19bf5eaf9dc7fe93fdb9a7d2ec91c51
Joachim Kasper <kasper@iMac5K.local>
2022-12-06 21:50:03 +0100

    Copy non git

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -3,14 +3,44 @@
    ========================================================================== */
 
 $(document).ready(function() {
-
   // FitVids init
   $("#main").fitVids();
+
+  // Sticky sidebar
+  var stickySideBar = function() {
+    var show =
+      $(".author__urls-wrapper button").length === 0
+        ? $(window).width() > 1024 // width should match $large Sass variable
+        : !$(".author__urls-wrapper button").is(":visible");
+    if (show) {
+      // fix
+      $(".sidebar").addClass("sticky");
+    } else {
+      // unfix
+      $(".sidebar").removeClass("sticky");
+    }
+  };
+
+  stickySideBar();
+
+  $(window).resize(function() {
+    stickySideBar();
+  });
 
   // Follow menu drop down
   $(".author__urls-wrapper button").on("click", function() {
     $(".author__urls").toggleClass("is--visible");
     $(".author__urls-wrapper button").toggleClass("open");
+  });
+
+  // Close search screen with Esc key
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+      if ($(".initial-content").hasClass("is--hidden")) {
+        $(".search-content").toggleClass("is--visible");
+        $(".initial-content").toggleClass("is--hidden");
+      }
+    }
   });
 
   // Search toggle
@@ -19,16 +49,41 @@ $(document).ready(function() {
     $(".initial-content").toggleClass("is--hidden");
     // set focus on input
     setTimeout(function() {
-      $("#search").focus();
+      $(".search-content input").focus();
     }, 400);
   });
 
-  // init smooth scroll
-  $("a").smoothScroll({ offset: -20 });
+  // Smooth scrolling
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    offset: 20,
+    speed: 400,
+    speedAsDuration: true,
+    durationMax: 500
+  });
+
+  // Gumshoe scroll spy init
+  if($("nav.toc").length > 0) {
+    var spy = new Gumshoe("nav.toc a", {
+      // Active classes
+      navClass: "active", // applied to the nav list item
+      contentClass: "active", // applied to the content
+
+      // Nested navigation
+      nested: false, // if true, add classes to parents of active link
+      nestedClass: "active", // applied to the parent items
+
+      // Offset & reflow
+      offset: 20, // how far from the top of the page to activate a content area
+      reflow: true, // if true, listen for reflows
+
+      // Event support
+      events: true // if true, emit custom events
+    });
+  }
 
   // add lightbox class to all image links
   $(
-    "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']"
+    "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif'],a[href$='.webp']"
   ).addClass("image-popup");
 
   // Magnific-Popup options
@@ -64,5 +119,18 @@ $(document).ready(function() {
     },
     closeOnContentClick: true,
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+
+  // Add anchors for headings
+  $('.page__content').find('h1, h2, h3, h4, h5, h6').each(function() {
+    var id = $(this).attr('id');
+    if (id) {
+      var anchor = document.createElement("a");
+      anchor.className = 'header-link';
+      anchor.href = '#' + id;
+      anchor.innerHTML = '<span class=\"sr-only\">Permalink</span><i class=\"fas fa-link\"></i>';
+      anchor.title = "Permalink";
+      $(this).append(anchor);
+    }
   });
 });
